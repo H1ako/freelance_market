659a8034847b71725569fb375108fdb343619a5e
h1ako <nikita-sobolev-wd@yandex.ru>
2023-12-14 23:29:50 +0400

    fixed bug

--- frontend/src/features/home/components/blocks/QuestionsAnswersBlock/index.tsx
+++ frontend/src/features/home/components/blocks/QuestionsAnswersBlock/index.tsx
@@ -15,13 +15,7 @@ export default function QuestionsAnswersBlock() {
           aliquam vitae in. Commodo congue sed vitae fames. Eleifend felis augue
           commodo massa mollis.
         </Accordion>
-        <Accordion title='Как получить свой первый заказ?'>
-          Paragraph: Lorem ipsum dolor sit amet consectetur. In eu elit netus
-          pretium urna purus rutrum habitant cras. Vitae enim id nec sit sed
-          aliquam vitae in. Commodo congue sed vitae fames. Eleifend felis augue
-          commodo massa mollis.
-        </Accordion>
-        <Accordion title='Легко ли начать?'>
+        <Accordion title='Как получить первый заказ?'>
           Paragraph: Lorem ipsum dolor sit amet consectetur. In eu elit netus
           pretium urna purus rutrum habitant cras. Vitae enim id nec sit sed
           aliquam vitae in. Commodo congue sed vitae fames. Eleifend felis augue
