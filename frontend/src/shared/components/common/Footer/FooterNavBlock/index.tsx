478d24721a36cd01e027594ddf3a20a86091cef8
h1ako <nikita-sobolev-wd@yandex.ru>
2023-12-17 20:54:08 +0400

    updated accordion

--- frontend/src/shared/components/common/Footer/FooterNavBlock/index.tsx
+++ frontend/src/shared/components/common/Footer/FooterNavBlock/index.tsx
@@ -4,6 +4,7 @@ import FooterNavList from './FooterNavList'
 import NavLink from '@/shared/components/ui/buttons/NavLink'
 import styles from './styles.module.scss'
 
+// TODO: make animation of moving hover bar
 export default function FooterNavBlock() {
   return (
     <nav aria-label='footer navigation' className={styles.footer__nav}>
